11da2ce354bf2fb0fc1322309f0b2316e53241fe
jpablozavalavalle <juan.zavalav@usil.pe>
2025-11-04 23:08:47 -0500

    commit agregar eliminar nodo

--- grafo.h
+++ grafo.h
@@ -1,8 +1,24 @@
-#include "vertice.h"
-#include "arista.h"
+#include "grafo.h"
 
-using namespace std;
+int main() {
+    Grafo g;
 
-class Grafo {
+    g.insertarVertice("C001", "Hospital Nacional", "Hospital", "Av. Salud 123");
+    g.insertarVertice("C002", "Laboratorio Central", "Laboratorio", "Jr. Biología 456");
+    g.insertarVertice("C003", "Clinica Primavera", "Clínica", "Av. Flores 789");
 
-};
+    g.mostrarCentros();
+
+    vector<string> idsEliminar = {"C001", "C003"};
+
+    for(const string &id : idsEliminar) {
+
+        cout << "\n--- Eliminando centro " << id << "---\n";
+        g.eliminarVertice(id);
+    }
+
+    cout << endl;
+    g.mostrarCentros();
+
+    return 0;
+}
